963ae54b15098eb922db526351a8a02fad7b7204
AiLab_superhy <AiLab_superhy@superhy-PC>
2014-05-27 22:03:42 +0800

    (2014.5.27)

---
+++ test/ims/weka/excavate/WekaFirstTest.java
@@ -0,0 +1,13 @@
+package ims.weka.excavate;
+
+import org.junit.Test;
+
+public class WekaFirstTest {
+
+	@Test
+	public void testWekaUse() throws Throwable {
+		WekaFirst wekaFirst = new WekaFirst();
+
+		wekaFirst.wekaUse();
+	}
+}
